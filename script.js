3934a470f16da8c316fe57c88b02f941f376681f
roken47 <69013261+roken47@users.noreply.github.com>
2020-10-10 13:57:11 -0500

    function multiply done 2 ways

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-function add(a, b) {}
+let add = (a, b) => a + b;
+function multiply(a, b) {
+  let count = 0;
+  let total = 0;
+  while (b > count) {
+    count++;
+    total = add(a, total);
+  }
+  return total;
+}
+// function multiply(a, b) {
+//   let count = 0;
+//   let runningTotal = 0;
+//   let total = 0;
+//   while (b > count) {
+//     count++;
+//     runnningTotal = add(a, 0);
+//     total += runningTotal;
+//   }
+//   return total;
+// }
+// console.log("Multiply Function: ", multiply(4, 3));
+// function power() {}
