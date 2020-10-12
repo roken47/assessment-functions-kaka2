4c4c10554edf5d17391f71738f55cc2638782362
roken47 <mradeboer7@gmail.com>
2020-10-12 17:39:14 -0500

    mishap in transferring my work from codepen.io

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function power(a, b) {
   let count = 0;
   let total = 1;
   if (a === 0) {
-    return 0
+    return 0;
   }
   while (b > count) {
     count = add(count, 1);
@@ -23,9 +23,9 @@ function power(a, b) {
 function factorial(a) {
   let total = a;
   if (a === 0 || a === 1) {
-      return 1
-      }
-  while (a > count) {
+    return 1;
+  }
+  while (a > 1) {
     a = add(a, -1);
     total = multiply(total, a);
   }
