734cd2fc453bcf511d37d05597c1a39bc958ff3d
roken47 <mradeboer7@gmail.com>
2020-10-11 21:11:19 -0500

    rough ver of factorial() returns 1

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function power(a, b) {
   }
   return total;
 }
+function factorial(a) {
+  //5! = 5 * 4 * 3 * 2 * 1 = 120
+  let count = 0;
+  let newNum = a;
+  let oldNum = a;
+  let total = a;
+  while (a > count) {
+    count = add(count, 1);
+    newNum = add(newNum, -1);
+    total = power(oldNum, newNum);
+    oldNum = add(oldNum, -1);
+  }
+  // currently getting 1 after 5! maybe needs an IF statement
+  return total;
+}
